aff506236187e661219ec2a4768d462f9a0cb843
Jenn de la Fuente <designs@jrosebud.com>
2018-03-13 01:59:34 -0700

    Added morgan to packages

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 //stuff that's required
-//const bodyParser = require('body-parser');
-//const cors = require('cors');
-//const errorhandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
 const express = require('express');
 
+const morgan = require('morgan');
 const apiRouter = require('./api/api');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//app.use(bodyParser.json());
-//app.use(cors());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/api', apiRouter);
 
-//app.use(errorhandler());
+app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log('Listening on port: ' + PORT);
