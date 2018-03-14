e705c7bf53dfb962cfe401a22e04063f7085f871
Jenn de la Fuente <designs@jrosebud.com>
2018-03-13 22:14:18 -0700

    Finished the rest of menus routes

--- api/menus.js
+++ api/menus.js
@@ -76,69 +76,63 @@ menusRouter.post('/', (req, res, next) => {
 		}
 	});
 });
-/*
-//put route for employees
+
+//put route for menus
 menusRouter.put('/:id', (req, res, next) => {
 	//variables for information
-	const name = req.body.employee.name;
-	const position = req.body.employee.position;
-	const wage = req.body.employee.wage;
-	const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+	const title = req.body.menu.title;
 	// check for empty fields
-	if(!name || !position || !wage) {
+	if(!title) {
 		return res.status(400).send();
 	} 
 	
-	const sql = "UPDATE Employee SET name = $name, position = $position, wage = $wage, is_current_employee = $isCurrentEmployee WHERE Employee.id = $employeeId";
+	const sql = "UPDATE Menu SET title = $title WHERE Menu.id = $menuId";
 	const values = {
-		$name: name,
-		$position: position,
-		$wage: wage,
-		$isCurrentEmployee: isCurrentEmployee,
-		$employeeId: req.params.id
+		$title: title,
+		$menuId: req.params.id
 	}
 	db.run(sql, values, function(error){
 		if(error){
 			next(error);
 		} else {
 			db.get(
-				`SELECT * FROM Employee WHERE Employee.id = ${req.params.id}`,
-				(error, employee) => {
-					res.status(200).json({employee: employee});
+				`SELECT * FROM Menu WHERE Menu.id = ${req.params.id}`,
+				(error, menu) => {
+					res.status(200).json({menu: menu});
 				});
 		}
 	});
 });
 
-//delete route for employees
+//delete route for menus
 menusRouter.delete('/:id', (req, res, next) => {
-	const sql = "UPDATE Employee SET is_current_employee = 0 WHERE Employee.id = $employeeId";
-	const values = {
-		$employeeId: req.params.id
-	}
-	
-	db.run(sql, values, function(error){
+	const query = "SELECT * FROM MenuItem WHERE MenuItem.menu_Id = $menuId";
+	const values = {$menuId: req.params.id};
+	db.all(query, values, (error, menuitems) => {
 		if(error){
 			next(error);
-		} else {
-			db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.id}`,
-			(error, employee) => {
-				res.status(200).json({employee: employee});
+		} else if(menuitems.length === 0){
+			db.run(`DELETE FROM Menu WHERE Menu.id = ${req.params.id}`,
+			(error, menu) => {
+				res.status(204).send();
 			});
+		} else {
+			return res.status(400).send();
 		}
-	});
-});
+	}); // end db.get
+}); 
 
-//timesheet stuff
-//timesheet params
-menusRouter.param('timesheetId', (req, res, next, timesheetId) => {
-  const sql = 'SELECT * FROM Timesheet WHERE Timesheet.id = $timesheetId';
-  const values = {$timesheetId: timesheetId};
-  db.get(sql, values, (error, timesheet) => {
+
+//menu items stuff
+//menu items params
+menusRouter.param('menuItemId', (req, res, next, menuItemId) => {
+  const sql = 'SELECT * FROM MenuItem WHERE MenuItem.id = $menuItemId';
+  const values = {$menuItemId: menuItemId};
+  db.get(sql, values, (error, menuItem) => {
     if (error) {
       next(error);
-    } else if (timesheet) {
-      req.timesheet = timesheet;
+    } else if (menuItem) {
+      req.menuItem = menuItem;
       next();
     } else {
       res.sendStatus(404);
@@ -146,107 +140,119 @@ menusRouter.param('timesheetId', (req, res, next, timesheetId) => {
   });
 }); 
 
-
-//get timesheet for employee
-menusRouter.get('/:employeeId/timesheets', (req, res, next) => {
-	db.all("SELECT * FROM Timesheet WHERE employee_id = $employeeId",
-	{
-		$employeeId: req.params.employeeId
-	},
-	(error, timesheets) => {
+//get menu items for menus
+menusRouter.get('/:menuId/menu-items', (req, res, next) => {
+	//getting all menu items for a specific menu id
+	const query = "SELECT * FROM MenuItem WHERE MenuItem.menu_Id = $menuId";
+	const values = {$menuId: req.params.menuId};
+	db.all(query, values, (error, menuitems) => {
 		if(error){
 			next(error);
-		} else if (timesheets) {
-			res.status(200).json({timesheets: timesheets});
+		} else if(menuitems.length === 0){
+			//menuitems is an empty array but I can't seem to send it
+			return res.send(menuitems);
+		} else if(menuitems){
+			db.all(`SELECT * FROM MenuItem WHERE MenuItem.menu_id = ${req.params.menuId}`,
+			(error, menuItems) => {
+				if(error){
+					next(error);
+				} else {
+					res.status(200).json({menuItems: menuItems});
+				}
+			});
 		} else {
 			return res.status(404).send();
 		}
-	});
+	}); // end db.get
 });
 
-//post timesheet for employee
-menusRouter.post('/:employeeId/timesheets', (req, res, next) => {
+//post menu items
+menusRouter.post('/:menuId/menu-items', (req, res, next) => {
 	//variables for information
-	const employeeId = req.params.employeeId;
-	const hours = req.body.timesheet.hours;
-	const rate = req.body.timesheet.rate;
-	const date = req.body.timesheet.date;
+	const name = req.body.menuItem.name,
+	description = req.body.menuItem.description,
+	inventory = req.body.menuItem.inventory,
+	price = req.body.menuItem.price,
+	menu_id = req.params.menuId;
 	// check for empty fields
-	if(!hours || !rate || !date) {
+	if(!name || !inventory || !price) {
 		return res.status(400).send();
 	} 
 	
-	// run the queries, yo
-	const sql = "INSERT INTO Timesheet (hours, rate, date, employee_id) VALUES ($hours, $rate, $date, $employeeId)";
+	const sql = "INSERT INTO MenuItem (name, description, inventory, price, menu_id) VALUES ($name, $description, $inventory, $price, $menu_id)";
 	const values = {
-		$hours: hours,
-		$rate: rate,
-		$date: date,
-		$employeeId: employeeId
+		$name: name,
+		$description: description,
+		$inventory: inventory,
+		$price: price,
+		$menu_id: menu_id
 	}
 	db.run(sql, values, function(error){
 		if(error){
 			next(error);
 		} else {
 			db.get(
-				`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`,
-				(error, timesheet) => {
-					res.status(201).json({timesheet: timesheet});
+				`SELECT * FROM MenuItem WHERE MenuItem.id = ${this.lastID}`,
+				(error, menuItem) => {
+					res.status(201).json({menuItem: menuItem});
 				});
 		}
-	});
+	}); 
 });
 
-//put timesheet
-menusRouter.put('/:employeeId/timesheets/:timesheetId', (req, res, next) => {
+//post route for menu items
+menusRouter.put('/:menuId/menu-items/:menuItemId', (req, res, next) => {
 	//variables for information
-	const employeeId = req.params.employeeId;
-	const hours = req.body.timesheet.hours;
-	const rate = req.body.timesheet.rate;
-	const date = req.body.timesheet.date;
-	const timesheetId = req.params.timesheetId;
+	const name = req.body.menuItem.name,
+	description = req.body.menuItem.description,
+	inventory = req.body.menuItem.inventory,
+	price = req.body.menuItem.price,
+	menu_id = req.params.menuId,
+	id = req.params.menuItemId;
 	// check for empty fields
-	if(!hours || !rate || !date) {
+	if(!name || !inventory || !price) {
 		return res.status(400).send();
 	} 
 	
-	const sql = "UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date, employee_id = $employeeId WHERE Timesheet.id = $timesheetId";
+	const sql = "UPDATE MenuItem SET name = $name, description = $description, inventory = $inventory, price = $price, menu_id = $menu_id WHERE MenuItem.id = $id";
 	const values = {
-		$hours: hours,
-		$rate: rate,
-		$date: date,
-		$employeeId: employeeId,
-		$timesheetId: timesheetId
+		$name: name,
+		$description: description,
+		$inventory: inventory,
+		$price: price,
+		$menu_id: req.params.menuId,
+		$id: req.params.menuItemId
 	}
 	db.run(sql, values, function(error){
 		if(error){
 			next(error);
 		} else {
 			db.get(
-				`SELECT * FROM Timesheet WHERE Timesheet.id = ${timesheetId}`,
-				(error, timesheet) => {
-					res.status(200).json({timesheet: timesheet});
+				`SELECT * FROM MenuItem WHERE MenuItem.id = ${req.params.menuItemId}`,
+				(error, menuItem) => {
+					res.status(200).json({menuItem: menuItem});
 				});
-		}
-	});
-});
-
-//delete route for timesheets
-menusRouter.delete('/:employeeId/timesheets/:timesheetId', (req, res, next) => {
-	const sql = "DELETE FROM Timesheet WHERE Timesheet.id = $timesheetId";
-	const values = {
-		$timesheetId: req.params.timesheetId
-	}
-	
-	db.run(sql, values, function(error){
-		if(error){
-			next(error);
-		} else {
-			res.status(204).send();
 		}
 	});
 }); 
-*/
+
+//delete route for menu items
+menusRouter.delete('/:menuId/menu-items/:menuItemId', (req, res, next) => {
+	const query = "SELECT * FROM MenuItem WHERE MenuItem.id = $menuItemId";
+	const values = {$menuItemId: req.params.menuItemId};
+	db.all(query, values, (error, menuitems) => {
+		if(error){
+			next(error);
+		} else if(menuitems){
+			db.run(`DELETE FROM MenuItem WHERE MenuItem.id = ${req.params.menuItemId}`,
+			(error, menu) => {
+				res.status(204).send();
+			});
+		} else {
+			return res.status(404).send();
+		}
+	}); // end db.get
+}); 
 
 //don't forget the export!!!!
 module.exports = menusRouter;
